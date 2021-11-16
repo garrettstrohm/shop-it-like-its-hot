da9bfd005c3a837e6cfc5fc2da26de1c9aee2ebf
Garrett <garres4@vt.edu>
2021-11-16 09:56:39 -0700

    fetched data

--- my-app/src/MainPage.js
+++ my-app/src/MainPage.js
@@ -2,8 +2,21 @@ import ItemContainer from './ItemContainer'
 import Sort from './Sort'
 import Cart from './Cart'
 import Filter from './Filter'
+import {useEffect, useState} from "react"
 
 function MainPage(){
+    const [products, setProducts] = useState([])
+    const[copyOfProducts, setCopyOfProducts] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:3000/products')
+        .then(r => r.json())
+        .then(productsArray => {
+            setProducts(productsArray)
+            setCopyOfProducts(productsArray)
+        })
+    }, [])
+
     return(
         <div>
         <ItemContainer />
