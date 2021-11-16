fe2c19c40a4130c3b3435916b130220b62d25572
Garrett <garres4@vt.edu>
2021-11-16 16:17:34 -0700

    hooked up forms

--- my-app/src/PaymentForm.js
+++ my-app/src/PaymentForm.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {useState} from 'react';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
@@ -6,6 +7,19 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 
 export default function PaymentForm() {
+const [paymentData, setPaymentData] = useState({
+    cardName: "",
+    cardNumber: "",
+    expDate: "",
+    ccv: ""
+})
+
+    function handleOnChange(e) {
+        setPaymentData({...paymentData, [e.target.name]: e.target.value})
+    }
+
+    console.log(paymentData)
+
   return (
     <React.Fragment>
       <Typography variant="h6" gutterBottom>
@@ -16,41 +30,53 @@ export default function PaymentForm() {
           <TextField
             required
             id="cardName"
+            name="cardName"
+            value={paymentData.cardName}
             label="Name on card"
             fullWidth
             autoComplete="cc-name"
             variant="standard"
+            onChange={handleOnChange}
           />
         </Grid>
         <Grid item xs={12} md={6}>
           <TextField
             required
             id="cardNumber"
+            name="cardNumber"
+            value={paymentData.cardNumber}
             label="Card number"
             fullWidth
             autoComplete="cc-number"
             variant="standard"
+            onChange={handleOnChange}
           />
         </Grid>
         <Grid item xs={12} md={6}>
           <TextField
             required
             id="expDate"
+            name="expDate"
+            value={paymentData.expData}
             label="Expiry date"
             fullWidth
             autoComplete="cc-exp"
             variant="standard"
+            onChange={handleOnChange}
           />
         </Grid>
         <Grid item xs={12} md={6}>
           <TextField
             required
             id="cvv"
+            name="ccv"
+            value={paymentData.ccv}
             label="CVV"
             helperText="Last three digits on signature strip"
             fullWidth
             autoComplete="cc-csc"
             variant="standard"
+            onChange={handleOnChange}
           />
         </Grid>
         <Grid item xs={12}>
