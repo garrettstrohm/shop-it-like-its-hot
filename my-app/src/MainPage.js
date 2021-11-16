2497d23aff4f868a8a7eee5ecd7f40f0ab3158a6
Nicholas Fagner <nicholasfagner@gmail.com>
2021-11-16 09:40:57 -0700

    MainPage Skeleton complete

---
+++ my-app/src/MainPage.js
@@ -0,0 +1,15 @@
+import ItemContainer from './ItemContainer'
+import Sort from './Sort'
+import Cart from './Cart'
+import Filter from './Filter'
+
+function MainPage(){
+    return(
+        <div>
+        <ItemContainer />
+        <Sort />
+        <Cart />
+        <Filter />
+        </div>
+    )
+}
