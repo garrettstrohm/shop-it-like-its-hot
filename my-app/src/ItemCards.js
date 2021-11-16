cb1f4ef5303b71677a2bdd3bb63e359e774bb7c7
BubbaGodinet <godinetvn@gmail.com>
2021-11-16 16:36:10 -0700

    simple card styling

--- my-app/src/ItemCards.js
+++ my-app/src/ItemCards.js
@@ -1,22 +1,41 @@
+import * as React from 'react';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
 import ItemDetails from "./ItemDetails"
 import Cart from "./Cart"
 
-
-
-function ItemCards({title, price, description, category, image, ratingObj, sendToCart}){
+export default function ItemCards({title, price, description, category, image, ratingObj, sendToCart}){
+   
 
     return(
-        <>
-        <div>
-            <h1>{title}</h1>
-            <img src = {image} alt = "imageAlt"style = {{maxHeight: 100, maxWidth: 100}}/>
-            <h2>${price} </h2>
-            <h3>{ratingObj.rate}⭐️</h3>
-            <button onClick= {sendToCart}>Add To Cart</button>
-            
-        </div>
-        </>
-    )
+        <Card style= {{backgroundColor: '#ffffff', border: '1px solid #1976D2'}}sx={{ display: 'flex', padding: '10px'}}>
+            <CardMedia
+            component="img"
+            sx={{ width: 151 }}
+            image={image}
+            alt="imgAlt"
+          />
+          <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+            <CardContent sx={{ flex: '1 0 auto' }}>
+              <Typography component="div" variant="h5">
+                {title}
+              </Typography>
+              <Typography variant="subtitle1" color="text.secondary" component="div">
+                 ${price} 
+              </Typography>
+              <Typography variant="subtitle1" color="text.secondary" component="div">
+                {ratingObj.rate}⭐️ 
+              </Typography>
+            </CardContent>
+            <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
+            </Box>
+          </Box>
+          
+        </Card>
+      );
 }
 
-export default ItemCards
