fb87e5e843dfafc9ea4f77488133e3e70639817f
Garrett <garres4@vt.edu>
2021-11-16 12:03:37 -0700

    got navbar working

--- my-app/src/MainPage.js
+++ my-app/src/MainPage.js
@@ -2,17 +2,17 @@ import ItemContainer from './ItemContainer'
 import Sort from './Sort'
 import Cart from './Cart'
 import Filter from './Filter'
-import NavBar from './NavBar'
 import {useEffect, useState} from "react"
+import NavBar from "./NavBar.js"
 
 function MainPage(){
     const [items, setItems] = useState([])
     const[copyOfItems, setCopyOfItems] = useState([])
     const [search, setSearch] = useState("")
     const[categorySelect, setCategorySelect] = useState("All")
-
+    console.log(search)
     useEffect(() => {
-        fetch('http://localhost:3000/products')
+        fetch('http://localhost:4000/products')
         .then(r => r.json())
         .then(itemsArray => {
             setItems(itemsArray)
