a7155f44aee8df25861de5dd972b0c1fd987af15
Garrett <garres4@vt.edu>
2021-11-17 11:20:54 -0700

    updated pages

--- my-app/src/Review.js
+++ my-app/src/Review.js
@@ -7,31 +7,10 @@ import Grid from '@mui/material/Grid';
 
 
 export default function Review({formData, products, totalPrice}) {
-  // const [products, setProducts] = useState([])
-  // const [total, setTotal] = useState(0)
+
   const { firstName, lastName, address1, address2, city, state, zip, country, cardName, cardNumber, expDate, ccv} = formData
   const lastFourDigits = cardNumber.substr(-4)
 
-  // useEffect(() => {
-  //   fetch('http://localhost:4000/cart')
-  //   .then(r => r.json())
-  //   .then(productsInCart => setProducts(productsInCart))
-  // }, [])
-  // {
-  //   "id": 20,
-  //   "title": "DANVOUY Womens T Shirt Casual Cotton Short",
-  //   "price": 12.99,
-  //   "description": "95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.",
-  //   "category": "women's clothing",
-  //   "image": "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg",
-  //   "rating": {
-  //     "rate": 3.6,
-  //     "count": 145
-  //   }
-  // }
-
-  
-  
   return (
     <React.Fragment>
       <Typography variant="h6" gutterBottom>
@@ -81,50 +60,3 @@ export default function Review({formData, products, totalPrice}) {
     </React.Fragment>
   );
 }
-
-// {
-  //   "id": 1,
-  //   "firstName": "",
-  //   "lastName": "",
-  //   "address1": "",
-  //   "address2": "",
-  //   "city": "",
-  //   "state": "",
-  //   "zip": "",
-  //   "country": "",
-  //   "cardName": "",
-  //   "cardNumber": "",
-  //   "expDate": "",
-  //   "ccv": ""
-  // }
-  // const products = [
-  //   {
-  //     name: 'Product 1',
-  //     desc: 'A nice thing',
-  //     price: '$9.99',
-  //   },
-  //   {
-  //     name: 'Product 2',
-  //     desc: 'Another thing',
-  //     price: '$3.45',
-  //   },
-  //   {
-  //     name: 'Product 3',
-  //     desc: 'Something else',
-  //     price: '$6.51',
-  //   },
-  //   {
-  //     name: 'Product 4',
-  //     desc: 'Best thing of all',
-  //     price: '$14.11',
-  //   },
-  //   { name: 'Shipping', desc: '', price: 'Free' },
-  // ];
-  
-  // const addresses = ['1 MUI Drive', 'Reactville', 'Anytown', '99999', 'USA'];
-  // const payments = [
-  //   { name: 'Card type', detail: 'Visa' },
-  //   { name: 'Card holder', detail: 'Mr John Smith' },
-  //   { name: 'Card number', detail: 'xxxx-xxxx-xxxx-1234' },
-  //   { name: 'Expiry date', detail: '04/2024' },
-  // ];
