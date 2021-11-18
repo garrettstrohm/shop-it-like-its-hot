d6e76b987f0d484ccbe98aaedbade8d031fd27ef
Nicholas Fagner <nicholasfagner@gmail.com>
2021-11-18 13:55:35 -0600

    Delete cart draft working

--- src/Cart.js
+++ src/Cart.js
@@ -6,7 +6,19 @@ import {CartContext} from './context/cartState'
 function Cart(){
     const {cart, setCart} = useContext(CartContext)
     
-    const cartItemsList = cart.map(item => <CartCards key={item.title} title={item.title} price={item.price} image={item.image} rating={item.rating.rate} description={item.description}/>)
+    const cartItemsList = cart.map(item => <CartCards key={item.title} id={item.id} title={item.title} price={item.price} image={item.image} rating={item.rating.rate} description={item.description} handleDelete={handleDelete} setCart={setCart}/>)
+
+    function handleDelete(id){
+        fetch(`http://localhost:4000/cart/${id}`, {method: "DELETE", headers:{"Content-type": "application/json"}})
+        .then(r => r.json())
+        .then(() => updateItemList(id))
+      
+    }
+
+    function updateItemList(id){
+        const updatedItemList = cart.filter((item) => item.id !== id)
+        setCart(updatedItemList)
+    }
 
     return(
         <>
