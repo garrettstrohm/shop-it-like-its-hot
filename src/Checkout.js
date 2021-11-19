cd018584aeed343bae93585810f1465aea2aab23
BubbaGodinet <godinetvn@gmail.com>
2021-11-19 14:16:04 -0700

    extra spacing

--- src/Checkout.js
+++ src/Checkout.js
@@ -169,7 +169,7 @@ function getStepContent(step) {
        
       >
       </AppBar>
-      <Container style = {{marginTop: '125px'}} component="main" maxWidth="sm" sx={{ mb: 4 }}>
+      <Container style = {{marginTop: '185px'}} component="main" maxWidth="sm" sx={{ mb: 4 }}>
         <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
           <Typography component="h1" variant="h4" align="center">
             Checkout
